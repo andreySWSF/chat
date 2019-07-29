e4396490b30c2455154975f228a09195e9fa8b4c
andreySWSF <andrey.swsf@gmail.com>
2019-07-29 16:44:13 +0300

    !!!!!!!!!!!!

--- Skype/ChatService/ChatHub.cs
+++ Skype/ChatService/ChatHub.cs
@@ -19,7 +19,8 @@ namespace Skype.ChatService
         readonly IMapper _mapper;
         UserManager<User> _userManager;
         SignInManager<User> _signInManager;
-       // User testUser = new User() { NickName = "dddd", Password = "000" };
+        static HashSet<string> CurrentConnections = new HashSet<string>();
+        // User testUser = new User() { NickName = "dddd", Password = "000" };
         IHttpContextAccessor _httpContextAccessor;
         public ChatHub(IMapper mapper, UserManager<User> userManager, SignInManager<User> signInManager)
         {
@@ -27,6 +28,43 @@ namespace Skype.ChatService
             _mapper = mapper;
             _userManager = userManager;
         }
+
+        public override Task OnConnectedAsync()
+        {
+            var id = Context.ConnectionId;
+            CurrentConnections.Add(id);
+
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception ex)
+        {
+            var connection = CurrentConnections.FirstOrDefault(x => x == Context.ConnectionId);
+
+            if (connection != null)
+            {
+                CurrentConnections.Remove(connection);
+            }
+
+            return base.OnDisconnectedAsync(ex);
+        }
+
+
+        //return list of all active connections
+        public List<string> GetAllActiveConnections()
+        {
+            return CurrentConnections.ToList();
+        }
+        //public void SendChatMessage(string who, string message)
+        //{
+        //    string name = Context.User.Identity.Name;
+
+        //    foreach (var connectionId in _connections.GetConnections(who))
+        //    {
+        //        Clients.Client(connectionId).addChatMessage(name + ": " + message);
+        //    }
+        //}
+
         public async Task Send(string message, string to)
         {
             var handler = new JwtSecurityTokenHandler();
@@ -43,7 +81,7 @@ namespace Skype.ChatService
                 var tokenS = handler.ReadToken(token) as JwtSecurityToken;
                 var claims = tokenS.Claims;
                 var name = tokenS.Claims.First(claim => claim.Type == ClaimsIdentity.DefaultNameClaimType).Value;
-               // var connectionId = Context.Co;
+                var connections = CurrentConnections.ToList();
 
                 if (name != to) // если получатель и текущий пользователь не совпадают
                    // await Clients.User(Context.UserIdentifier).SendAsync("Receive", message, name);
